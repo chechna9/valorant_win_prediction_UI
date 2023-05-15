9b575a5a1df44e23f07c133d5c31342715887fc2
chechna9 <ja_bouchouareb@esi.dz>
2023-05-15 20:00:43 +0100

    added assets

--- src/screens/simulation/add_agent_box.tsx
+++ src/screens/simulation/add_agent_box.tsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
+import { addIcon } from "../../utils/assets";
 
 const AddAgentBox = () => {
   const [chosen, setChosen] = useState(false);
-  return <div className="">add agent</div>;
+  return <div className="w-28 h-28 bg-myDark2 flex flex-col justify-center items-center">
+    {
+        !chosen && (
+            <button>
+                <img src={addIcon} alt="add agent" />
+            </button>
+        )
+    }
+  </div>;
 };
 
 export default AddAgentBox;
---
+++ src/utils/assets.ts
@@ -0,0 +1,52 @@
+import addIcon from "../assets/icons/addIcon.svg";
+import jett from "../assets/images/agents/jett.png";
+import chamber from "../assets/images/agents/chamber.png";
+import sova from "../assets/images/agents/sova.png";
+import viper from "../assets/images/agents/viper.png";
+import skye from "../assets/images/agents/sky.png";
+import astra from "../assets/images/agents/astra.png";
+import raze from "../assets/images/agents/raze.png";
+import sage from "../assets/images/agents/sage.png";
+import kayo from "../assets/images/agents/kayo.png";
+import killjoy from "../assets/images/agents/killjoy.png";
+import reyna from "../assets/images/agents/reyna.png";
+import cypher from "../assets/images/agents/cypher.png";
+import breach from "../assets/images/agents/breach.png";
+import omen from "../assets/images/agents/omen.png";
+import brimstone from "../assets/images/agents/brim.png";
+import phoenix from "../assets/images/agents/phonix.png";
+import yoru from "../assets/images/agents/yoru.png";
+import ascent from "../assets/images/maps/ascent.png";
+import bind from "../assets/images/maps/bind.png";
+import breeze from "../assets/images/maps/breeze.png";
+import fracture from "../assets/images/maps/fracture.png";
+import heaven from "../assets/images/maps/heaven.png";
+import icebox from "../assets/images/maps/icebox.png";
+import split from "../assets/images/maps/split.png";
+export {
+  addIcon,
+  astra,
+  breach,
+  brimstone,
+  chamber,
+  cypher,
+  jett,
+  kayo,
+  killjoy,
+  omen,
+  phoenix,
+  raze,
+  reyna,
+  sage,
+  skye,
+  sova,
+  viper,
+  yoru,
+  ascent,
+  bind,
+  breeze,
+  fracture,
+  heaven,
+  icebox,
+  split,
+};
