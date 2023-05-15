f4c78efe02b2d9abeb5eb6f642eaac189d990299
chechna9 <ja_bouchouareb@esi.dz>
2023-05-15 18:15:17 +0100

    removed material design and replaced the switch compoenent

---
+++ src/screens/simulation/add_agent_box.tsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const AddAgentBox = () => {
+  const [chosen, setChosen] = useState(false);
+  return <div className="">add agent</div>;
+};
+
+export default AddAgentBox;
--- src/screens/simulation/team_selection.tsx
+++ src/screens/simulation/team_selection.tsx
@@ -1,7 +1,8 @@
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import Switch from "@mui/material/Switch";
+
 import { AgentEnum } from "../../utils/enums/agents_enum";
 import { all_agents } from "../../utils/consts";
+import AddAgentBox from "./add_agent_box";
+import ToggleSwitch from "../../components/toggle";
 
 interface props {
   team: String;
@@ -15,32 +16,15 @@ const TeamSelection: React.FC<props> = (props) => {
         <h1 className="text-white text-2xl font-bold">Team {props.team}</h1>
         <div>
           <h2 className="text-white font-medium">Attacks first</h2>
-          <Switch
-            checked={props.isAttacker}
-            onClick={() => props.switchAttack()}
-          />
+         <ToggleSwitch
+         checked ={ props.isAttacker}
+         onClick={props.switchAttack}
+         />
         </div>
       </div>
       {/* select agents */}
       <div>
-        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-          <InputLabel id="demo-simple-select-label">Select</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={"f"}
-            label="Agent"
-            // onChange={handleChange}
-          >
-            {/* <MenuItem value={10}>Ten</MenuItem> */}
-            {
-                all_agents.map((e)=><MenuItem value={e.agent}>{e.agent}</MenuItem>)
-            }
-            
-            
-          </Select>
-        </FormControl>
-       
+       <AddAgentBox/>
       </div>
     </div>
   );
