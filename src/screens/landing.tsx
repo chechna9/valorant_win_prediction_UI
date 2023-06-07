e974245136bebba12a821fa73a615c9a15aafc78
amir-coder <ja_amir@esi.dz>
2023-06-07 15:44:35 +0200

    finished landing first page

--- src/screens/landing.tsx
+++ src/screens/landing.tsx
@@ -1,14 +1,20 @@
 
 import valoImage from '../assets/images/valorant.jpg';
-
+import CustomButton  from '../components/custom_button';
+import { Outlet, Link } from "react-router-dom";
 const LandingPage = ()=>{
     return (
-      <div className="App bg-myDark h-screen">
-      <p className='text-white text-xl absolute z-10 top-0 right-0 rotate-45 pt-5' >BETA</p>
+      <div className='App bg-myDark overflow-clip  text-white'>
+<div className=" h-screen">
+        
+        <h1 className="text-white absolute right-2 top-2 rotate-45">
+            Beta
+          </h1>
+      {/* <p className='text-white text-xl absolute z-10 top-0 right-0 rotate-45 pt-5' >BETA</p> */}
       <header className=' w-full'>
         <div className="hero flex align-middle flex-wrap-reverse w-screen xl:px-28 px-10 lg:pt-48 pt-24">
           {/* Header */}
-            <div className='lg:w-1/2 lg:pr-10 text-white lg:text-left lg:text-3xl sm:text-2xl text-xl text-center '>
+            <div className='lg:w-1/2 lg:pr-10 lg:text-left lg:text-3xl sm:text-2xl text-xl text-center '>
               <h3 className='font-bold pb-6'>About The project: </h3>
               <p>
                 Find your 
@@ -20,8 +26,28 @@ const LandingPage = ()=>{
             <img src={valoImage} className="md:hero-image md:w-2/5 w-3/4 pb-12 lg:p-0" alt="logo" />
         </div>
         {/* action btn */}
+          {/* simulate */}
+          <div className="flex justify-center py-32">
+            <Link to="/">
+              <CustomButton>
+                Try It Now!
+              </CustomButton>
+            </Link>
+          </div>
       </header>
       </div>
+      <div className='min-h-screen w-screen xl:px-48 px-10 lg:pt-10 pt-10 lg:text-left lg:text-3xl sm:text-2xl text-xl text-center'>
+      <h3 className='font-bold pb-6'>It's Open Source! </h3>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <div className="bg-gray-200 p-4">Square 1</div>
+        <div className="bg-gray-200 p-4">Square 2</div>
+        <div className="bg-gray-200 p-4">Square 3</div>
+        <div className="bg-gray-200 p-4">Square 4</div>
+      </div>  
+
+        </div>
+      </div>
+      
     );
 }
 
