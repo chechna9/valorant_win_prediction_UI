dac13b00c1bb769afd66f25ad1b129d24ead5ffa
amir-coder <ja_amir@esi.dz>
2023-06-07 18:07:03 +0200

    fixing responsitivity

--- src/screens/landing.tsx
+++ src/screens/landing.tsx
@@ -1,5 +1,8 @@
 
-import valoImage from '../assets/images/valorant.jpg';
+import valoImage from '../assets/images/landing/valorant.jpg';
+import githubImage from '../assets/images/landing/github-mark-white.svg';
+import gcolabImage from '../assets/images/landing/Google_Colaboratory_SVG_Logo.svg';
+import kaggleImage from '../assets/images/landing/Kaggle_logo.png';
 import CustomButton  from '../components/custom_button';
 import { Outlet, Link } from "react-router-dom";
 const LandingPage = ()=>{
@@ -36,16 +39,36 @@ const LandingPage = ()=>{
           </div>
       </header>
       </div>
-      <div className='min-h-screen w-screen xl:px-48 px-10 lg:pt-10 pt-10 lg:text-left lg:text-3xl sm:text-2xl text-xl text-center'>
+      <div className='min-h-screen w-screen xl:px-48 px-10 lg:pt-10 pt-6 lg:text-left lg:text-3xl sm:text-2xl text-xl text-center flex flex-col justify-center align-center '>
       <h3 className='font-bold pb-6'>It's Open Source! </h3>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <div className="bg-gray-200 p-4">Square 1</div>
-        <div className="bg-gray-200 p-4">Square 2</div>
-        <div className="bg-gray-200 p-4">Square 3</div>
-        <div className="bg-gray-200 p-4">Square 4</div>
-      </div>  
+      <div className='max-w-fit mx-auto'>
+        <div className="grid justify-around grid-cols-1 gap-4 sm:grid-cols-2 ">
+          <div className="bg-myDark2 lg:w-64 w-32 lg:h-64 h-32 p-10 justify-between flex-col">
+            <img src={githubImage} className="lg:w-2/3 w-full mx-auto block" alt="Github" />
+            <h3 className='font-bold text-xs lg:text-sm w-full text-center bg-myDark '>Source Code</h3>
+          </div>
+          <div className="bg-myDark2  lg:w-64 w-32 lg:h-64 h-32 p-4">
+            <img src={gcolabImage} className="px-8 w-full" alt="CO" />
+            <h3 className='font-bold text-sm w-full text-center py-16'>NoteBook</h3></div>
+          <div className="bg-myDark2  lg:w-64 w-32 lg:h-64 h-32 p-4">
+            <img src={githubImage} className="px-8 w-full" alt="Github" />
+            <h3 className='font-bold text-sm w-full text-center py-16'>Project Report</h3></div>
+          <div className="bg-myDark2  lg:w-64 w-32 lg:h-64 h-32 p-4">
+            <img src={kaggleImage} className="px-8 w-full" alt="Github" />
+            <h3 className='font-bold text-sm w-full text-center py-16'>Dataset</h3>
+            </div>
+        </div>
+      </div>
+
 
         </div>
+        <div className='min-h-screen w-screen xl:px-48 px-10 lg:pt-10 pt-6 lg:text-left lg:text-3xl sm:text-2xl text-xl text-center'>
+      <h3 className='font-bold pb-6'>Made With Coffee By</h3>
+
+
+        </div>
+
+
       </div>
       
     );
