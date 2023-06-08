d416b72555f6b9c964721e7783f633292e70fb66
amir-coder <ja_amir@esi.dz>
2023-06-08 11:51:04 +0200

    initial made by section

--- src/screens/landing.tsx
+++ src/screens/landing.tsx
@@ -4,19 +4,20 @@ import githubImage from '../assets/images/landing/github-mark-white.svg';
 import gcolabImage from '../assets/images/landing/Google_Colaboratory_SVG_Logo.svg';
 import kaggleImage from '../assets/images/landing/Kaggle_logo.png';
 import driveImage from '../assets/images/landing/drive.png';
+import amir from '../assets/images/landing/amir.jpg';
+import yacine from '../assets/images/landing/yacine.jpg';
 import CustomButton  from '../components/custom_button';
 import { Outlet, Link } from "react-router-dom";
 const LandingPage = ()=>{
     return (
       <div className='App bg-myDark overflow-clip  text-white'>
-<div className=" h-screen">
+<div className="h-screen">
         
         <h1 className="text-white absolute right-2 top-2 rotate-45">
             Beta
           </h1>
-      {/* <p className='text-white text-xl absolute z-10 top-0 right-0 rotate-45 pt-5' >BETA</p> */}
       <header className=' w-full'>
-        <div className="hero flex align-middle flex-wrap-reverse w-screen xl:px-28 px-10 lg:pt-48 pt-24">
+        <div className="hero flex align-middle flex-wrap-reverse w-screen lg:px-48 px-10 lg:pt-48 pt-24">
           {/* Header */}
             <div className='lg:w-1/2 lg:pr-10 lg:text-left lg:text-3xl sm:text-2xl text-xl text-center '>
               <h3 className='font-bold pb-6'>About The project: </h3>
@@ -40,7 +41,7 @@ const LandingPage = ()=>{
           </div>
       </header>
       </div>
-      <div className='min-h-screen w-screen xl:px-48 px-10 lg:pt-10 pt-6 lg:text-left lg:text-3xl sm:text-2xl text-xl text-center flex flex-col justify-center align-center '>
+      <div className='min-h-screen w-screen lg:px-48 px-10 lg:pt-10 pt-6 lg:text-left lg:text-3xl sm:text-2xl text-xl text-center flex flex-col justify-center align-center '>
       <h3 className='font-bold pb-6'>It's Open Source! </h3>
       <div className='max-w-fit mx-auto'>
         <div className="grid justify-around grid-cols-1 gap-4 sm:grid-cols-2 ">
@@ -71,10 +72,18 @@ const LandingPage = ()=>{
 
 
         </div>
-        <div className='min-h-screen w-screen xl:px-48 px-10 lg:pt-10 pt-6 lg:text-left lg:text-3xl sm:text-2xl text-xl text-center'>
+        <div className='min-h-screen w-screen lg:px-48 px-10 lg:pt-10 pt-6 lg:text-left lg:text-3xl sm:text-2xl text-xl text-center'>
       <h3 className='font-bold pb-6'>Made With Coffee By</h3>
-
-
+      <div className='grid grid-cols-1 lg:gap-20 gap-4 lg:grid-cols-2 h-full justify-between'>
+          <div className="h-full p-2 grid content-start bg-myDark2">
+            <img src={yacine} className="h-fill mx-auto" alt="Yacine" />
+            <h3 className='font-bold text-xs lg:text-sm w-full text-center lg:mt-10 mt-2'>Ahmed Yacine Bouchouareb</h3>
+          </div>
+          <div className="h-full p-2 grid content-start bg-myDark2">
+            <img src={amir} className="h-full mx-auto" alt="Amir" />
+            <h3 className='font-bold text-xs lg:text-sm w-full text-center lg:mt-10 mt-2'>Amir Almamma</h3>
+          </div>
+      </div>
         </div>
 
 
