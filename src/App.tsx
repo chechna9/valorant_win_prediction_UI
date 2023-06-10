e82d23f4ac000dd7cc03466eff8c449521aecb53
chechna9 <ja_bouchouareb@esi.dz>
2023-06-10 18:42:44 +0100

    fixed routes and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SimulationPage from './screens/simulation/simulation';
-import LandingPage from './screens/landing';
-
+import SimulationPage from "./screens/simulation/simulation";
+import LandingPage from "./screens/landing";
 
 export default function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<SimulationPage />} />
-      <Route path="home" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/prediction" element={<SimulationPage />} />
       </Routes>
     </BrowserRouter>
   );
